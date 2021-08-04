3218bd4d70abce14ff357b1cd712314ab1c3b1e9
9956 <zhz995617@gmail.com>
2021-08-04 22:54:24 +0800

    chore: simple-cli move to packages

--- packages/simple-cli/src/command.ts
+++ packages/simple-cli/src/command.ts
@@ -7,13 +7,13 @@ const packageJSON = require('../package.json');
 const cli: InterfaceCLI = new Command();
 
 cli
-	.description('mini cli...')
+	.description('simple cli...')
 	.version(packageJSON.version, '-v --version')
 	.option('-i --init <project>', 'init project')
 	.option('-d --debug', 'show debug info')
 	.action((options: InterfaceCLI) => handleOptions(options))
 	.on('--help', () => {
 		console.log('Example call:');
-		console.log(' $ mini --help');
+		console.log(' $ simple --help');
 	})
 	.parse(process.argv);
--- packages/simple-cli/src/index.ts
+++ packages/simple-cli/src/index.ts
@@ -1,3 +1,3 @@
 import 'module-alias/register';
 
-import '@/command'
+import '@/command';
